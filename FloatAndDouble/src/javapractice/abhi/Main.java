6bc082656baec429d7b3248bdc304e494b9353ab
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-03-12 23:12:54 +1000

    completed the float and double working on the char and boolean now

--- FloatAndDouble/src/javapractice/abhi/Main.java
+++ FloatAndDouble/src/javapractice/abhi/Main.java
@@ -14,12 +14,24 @@ public class Main {
         System.out.println("Minimum Double Value = " + myMinimumDoubleValue);
         System.out.println("Maximum Double Value = " + myMaximumDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
-        System.out.println(myFloatValue);
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
 
-        
-        
+        double numberOfPound = 200d;
+        double convertedKilogram = numberOfPound * 0.45359237d;
+        System.out.println("numberOfPound = " + numberOfPound);
+        System.out.println("Coverted kilogram = " + convertedKilogram);
+
+        double pi = 3.1415926535d;
+        System.out.println("pi = " + pi);
+
+
+
+
+
     }
 }
