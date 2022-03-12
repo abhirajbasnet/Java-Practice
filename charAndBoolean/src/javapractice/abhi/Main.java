ba9ceaeba566683682eb4aeaf68c2e47adb25459
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-03-13 00:36:31 +1000

    learned unique code and boolean

--- charAndBoolean/src/javapractice/abhi/Main.java
+++ charAndBoolean/src/javapractice/abhi/Main.java
@@ -3,6 +3,19 @@ package javapractice.abhi;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        char myCharValue = 'd';
+        char myUnicodeValue = '\u27B3';
+        System.out.println("My char value = " + myCharValue);
+        System.out.println("My unicode value = " + myUnicodeValue);
+
+        boolean isStudentArePresent = true;
+        boolean myFalseBoolean = false;
+        boolean myTrueBoolean = true;
+        System.out.println(isStudentArePresent);
+        System.out.println(myFalseBoolean);
+        System.out.println(myTrueBoolean);
+
+
     }
 }
