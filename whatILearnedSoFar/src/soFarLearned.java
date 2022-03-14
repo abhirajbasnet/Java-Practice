197aad7c11b2057f3f4e71ed3ee9e9df8214ceb4
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-03-14 23:34:32 +1000

    had some code on the whatever i learned from

--- whatILearnedSoFar/src/soFarLearned.java
+++ whatILearnedSoFar/src/soFarLearned.java
@@ -2,16 +2,56 @@ public class soFarLearned {
 
     public static void main(String[] arg){
 
-        System.out.println("Hello, Abhiraj");
+        //main code for the java code
+        /*
+         * Variable are the way of storing the data in the computer memory by the given data type and name by us where computer does the hard part
+         * whatever inside the parenthesis is the parameter, period
+         * Different theory, method class, data type, primitive type, wrapper class,
+         * There are 8 primitive data types int, long, double, boolean, float, short, char, byte
+         * wrapper class are those class of the primitive type which helps to operate the java functions of the primitive type
+         * Java operative simply known as (+ - \ *)
+         *
+         */
 
-        int myRollNumber = 4;
-        int myFirstNumber = 10;
-        int myLastNumber = 2;
-        int myTotalNumber = myFirstNumber + myRollNumber + myLastNumber;
-        System.out.println(myTotalNumber);
+        //short, Byte, int, Long
 
-        int myClassNumber = 1000 - myTotalNumber;
-        System.out.println(myClassNumber);
+        byte myMinimumByteValue = Byte.MIN_VALUE;
+        byte myMaximumByteValue = Byte.MAX_VALUE;
+        System.out.println("Minimum Byte Value = " + myMinimumByteValue);
+        System.out.println("Maximum Byte Value = " + myMaximumByteValue);
+
+        short myMinimumShortValue = Short.MIN_VALUE;
+        short myMaximumShortValue = Short.MAX_VALUE;
+        System.out.println("Minimum Short Value = " + myMinimumShortValue);
+        System.out.println("Maximum Short Value = " + myMaximumShortValue);
+
+        int myMinimumIntValue = Integer.MIN_VALUE;
+        int myMaximumIntValue = Integer.MAX_VALUE;
+        System.out.println("Minimum Int Value = " + myMinimumIntValue);
+        System.out.println("Maximum Int Value = " + myMaximumIntValue);
+
+        int myIntOverFlowValue = 2147483647 + 1; //perfect example of the overflow it is  because int cannot store more than its maximum storage so its loses the data and print as -
+        System.out.println(myIntOverFlowValue);
+        int myIntUnderFlowValue = -2147483647 - 1; // example of the underflow
+        System.out.println(myIntUnderFlowValue);
+        //so to counter attack the problem of int we use long
+
+        long myMinimumLongValue = Long.MIN_VALUE;
+        long myMaximumLongValue = Long.MAX_VALUE;
+        System.out.println("Minimum Long Value = " + myMinimumLongValue);
+        System.out.println("Maximum Long Value = " + myMaximumLongValue);
+
+        //Casting which means to treat or convert a number from one type to another
+        int value = 50000;
+
+
+
+
+
+
+
+
+
 
     }
 }
