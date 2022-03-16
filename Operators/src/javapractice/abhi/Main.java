daccee8a196798caa7bc95c39aea70f023a63a8a
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-03-16 21:38:04 +1000

    working on conditional logic if-then

--- Operators/src/javapractice/abhi/Main.java
+++ Operators/src/javapractice/abhi/Main.java
@@ -43,8 +43,11 @@ public class Main {
         result -= 2; // 10 - 2 = 8
         System.out.println("10 - 2 = " + result );
 
-
-
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an Alien");
+            System.out.println("I am afraid of an Alien");
+        }
 
 
 
