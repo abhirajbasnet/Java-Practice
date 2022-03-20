d0aa53cf0074c444f05466f13eb76e97c8880393
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-03-21 03:21:59 +1000

    worked on keyword and expression

--- keywordAndExpression/src/javapractice/abhi/Main.java
+++ keywordAndExpression/src/javapractice/abhi/Main.java
@@ -3,6 +3,19 @@ package javapractice.abhi;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344d);
+        System.out.println("Converted to the kilometers" + kilometers);
+
+        int highScore = 50;
+
+        if (highScore == 50){
+            System.out.println("This is an expression");
+        }
+        
+
+
+
+
     }
 }
