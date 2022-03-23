2ae5e130259361d77927b2fd0dd85677bc0c7558
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-03-24 09:48:24 +1000

    learned alternate way of returning value

--- methods/src/javapractice/abhi/Main.java
+++ methods/src/javapractice/abhi/Main.java
@@ -48,15 +48,23 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
-        if (playerScore >= 1000) {
-            return 1;
-        } else if (playerScore >= 500) {
-            return 2;
-        } else if (playerScore >= 100) {
-            return 3;
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100) {
+//            return 3;
+//        }
+//        return 4;
+        int position = 4; //assuming position 4 will be returned
+        if(playerScore >=1000){
+            position = 1;
+        }else if(playerScore >=500){
+            position = 2;
+        }else if(playerScore >=100){
+            position = 3;
         }
-
-        return 4;
+        return position;
     }
 
 }
