a44ed11a13190829238546f415eb264f0fc46cc5
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-03-24 14:18:19 +1000

    completed the project

--- PrintMegaBytesAndKiloBytes/src/MegaBytesConverter.java
+++ PrintMegaBytesAndKiloBytes/src/MegaBytesConverter.java
@@ -1,2 +1,16 @@
 public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+
+        int megabytes = kiloBytes / 1024;// 1 MB = 1024 KB
+        // System.out.println("MegaByte = " + megabytes + "Mb");
+        int convertedKiloByte = kiloBytes % 1024;
+
+        if (kiloBytes <= 0){
+            System.out.println("Invalid Value");
+        }else {
+
+            System.out.println(kiloBytes + " KB " + "= " + megabytes + " MB" + " and " + convertedKiloByte + " KB" );
+        }
+    }
 }
