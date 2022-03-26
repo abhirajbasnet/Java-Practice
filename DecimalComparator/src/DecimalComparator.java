febbe5297deb3a444041d5413fff9034c5681ff5
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-03-26 22:57:16 +1000

    wprked on the decimal converter

--- DecimalComparator/src/DecimalComparator.java
+++ DecimalComparator/src/DecimalComparator.java
@@ -1,3 +1,16 @@
 public class DecimalComparator {
-    
+
+    public static boolean areEqualByThreeDecimal(double equal, double threeDecimal){
+        int a = (int) equal * 1000;
+        int b = (int) threeDecimal * 1000;
+
+        if(a == b){
+            System.out.println(true);
+            return true;
+        }
+        else
+
+            System.out.println(false);
+        return false;
+    }
 }
--- DecimalComparator/src/Main.java
+++ DecimalComparator/src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+
+    public static void main(String[] args){
+
+        boolean isDecimalByThree = DecimalComparator.areEqualByThreeDecimal(3.33, 4.33);
+        System.out.println(isDecimalByThree);
+        isDecimalByThree = DecimalComparator.areEqualByThreeDecimal(-3.1756, -3.175);
+        System.out.println(isDecimalByThree);
+        isDecimalByThree = DecimalComparator.areEqualByThreeDecimal(3.175, 3.176);
+        System.out.println(isDecimalByThree);
+        isDecimalByThree = DecimalComparator.areEqualByThreeDecimal(3.0, 3.0);
+        System.out.println(isDecimalByThree);
+        isDecimalByThree = DecimalComparator.areEqualByThreeDecimal(-3.123, 3.123);
+        System.out.println(isDecimalByThree);
+    }
 }
