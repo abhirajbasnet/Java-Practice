cfecc92e0dc232705d9880830aa4c82ab59295c2
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-01 09:49:34 +1000

    completed the challenge

--- FoeElse/src/javapractice/abhi/Main.java
+++ FoeElse/src/javapractice/abhi/Main.java
@@ -3,6 +3,27 @@ package javapractice.abhi;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 5.0));
+
+        for (int i = 0; i < 5; i++){
+
+            System.out.println("Loop " + i + " hello!");
+
+        }
+
+        for(int i = 2; i < 9; i++ ){
+
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)) );
+        }
     }
+
+    public static double calculateInterest(double amount, double interestRate){
+
+        return (amount * (interestRate / 100));
+    }
+
 }
