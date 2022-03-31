fcc202716110ce89766d9c5836b5b81e64f63872
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-03-31 16:52:35 +1000

    completed a challenge

--- SwitchChallenge/src/javapractice/abhi/Main.java
+++ SwitchChallenge/src/javapractice/abhi/Main.java
@@ -3,6 +3,16 @@ package javapractice.abhi;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        WeekDays.printDayOfTheWeek(0);
+        WeekDays.printDayOfTheWeek(1);
+        WeekDays.printDayOfTheWeek(2);
+        WeekDays.printDayOfTheWeek(3);
+        WeekDays.printDayOfTheWeek(4);
+        WeekDays.printDayOfTheWeek(5);
+        WeekDays.printDayOfTheWeek(6);
+        WeekDays.printDayOfTheWeek(7);
+
+
     }
 }
