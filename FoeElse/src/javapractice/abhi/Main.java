45900960bf8a2cc8b999f75887d41354cdc6bf19
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-01 10:36:52 +1000

    completed a challenge

--- FoeElse/src/javapractice/abhi/Main.java
+++ FoeElse/src/javapractice/abhi/Main.java
@@ -1,5 +1,7 @@
 package javapractice.abhi;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -19,6 +21,50 @@ public class Main {
 
             System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)) );
         }
+
+        System.out.println("******************");
+
+        //How would you modify the for loop above to do the same thing as
+        // Shown but to start from 8% and way back to 2%
+
+        for ( int i = 8; i >= 2; i--){
+
+            System.out.println("10,000 " + i + "% interest = " + String.format("%.2f", calculateInterest(10000, i)));
+        }
+        System.out.println("********************");
+
+        int count = 0;
+        for( int i = 10; i <= 50; i ++ ){
+
+            if(isPrime(i)){
+                count ++;
+                System.out.println("Number " + i + " is a prime number");
+                if(count == 10){
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+
+        }
+
+
+
+    }
+
+    public static boolean isPrime(int n){
+
+        if (n == 1){
+            return false;
+        }
+        for (int i = 2; i <= (long) Math.sqrt(n); i++){
+
+            System.out.println("Looping " + i);
+
+            if(n % i ==0){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static double calculateInterest(double amount, double interestRate){
