75675af718b9e8107ae821b94b565c7db2db7ee8
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-03 12:11:14 +1000

    completed a project to calculate the even number in a digit

--- EvenDigitSum/src/EvenDigitSum.java
+++ EvenDigitSum/src/EvenDigitSum.java
@@ -1,2 +1,26 @@
 public class EvenDigitSum {
+
+    public static int getEvenDigitSum(int number) {
+
+        int beginingNumber = number;
+        int sumEvenDigit = 0;
+        if(number >=0){
+            while (number > 0){
+                int lastDigit = number % 10;
+                if (lastDigit % 2 == 0){
+                    sumEvenDigit += lastDigit;
+                }
+                number = number / 10;
+            }
+
+        }
+        if(number < 0){
+            System.out.println("Invalid Parameter");
+            return -1;
+        }
+
+        System.out.println("The sum of even number of " + beginingNumber + " is " + sumEvenDigit);
+
+        return sumEvenDigit;
+    }
 }
