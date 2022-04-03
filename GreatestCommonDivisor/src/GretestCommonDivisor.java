237bd2839744ad6c8c16b399c474e5224cf9cb50
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-04 00:54:31 +1000

    Working on the project

---
+++ GreatestCommonDivisor/src/GretestCommonDivisor.java
@@ -0,0 +1,14 @@
+public class GretestCommonDivisor {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int getGreatestCommonDivisor(int first, int second){
+
+        if(first < 10 || second < 10){
+
+            return -1;
+        }
+    }
+}
