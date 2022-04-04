89fac84da648e7c88596481e46c784365e7e1353
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-05 00:29:01 +1000

    reformated a code

--- GreatestCommonDivisor/src/GretestCommonDivisor.java
+++ GreatestCommonDivisor/src/GretestCommonDivisor.java
@@ -10,24 +10,18 @@ public class GretestCommonDivisor {
 
         int commonDivisor = 1;
 
-
-
         if (first >= 10 && second >= 10) {
 
 
-            for (int i = 1; ((i <= first) && (i <=second)); i++) {
+            for (int i = 1; ((i <= first) && (i <= second)); i++) {
 
-
-                if ((first % i == 0) && (second % i ==0)) {
+                if ((first % i == 0) && (second % i == 0)) {
 
                     commonDivisor = i;
-
                 }
-
             }
             return commonDivisor;
         }
-
         return -1;
     }
 }
