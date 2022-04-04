7bc5ca8d69a0e94a99614e02f2c34dde6a2f6219
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-05 00:16:14 +1000

    completed a project

--- GreatestCommonDivisor/src/GretestCommonDivisor.java
+++ GreatestCommonDivisor/src/GretestCommonDivisor.java
@@ -2,13 +2,32 @@ public class GretestCommonDivisor {
 
     public static void main(String[] args) {
 
+        System.out.println(getGreatestCommonDivisor(25, 15));
+
     }
 
-    public static int getGreatestCommonDivisor(int first, int second){
+    public static int getGreatestCommonDivisor(int first, int second) {
 
-        if(first < 10 || second < 10){
+        int commonDivisor = 1;
 
-            return -1;
+
+
+        if (first >= 10 && second >= 10) {
+
+
+            for (int i = 1; ((i <= first) && (i <=second)); i++) {
+
+
+                if ((first % i == 0) && (second % i ==0)) {
+
+                    commonDivisor = i;
+
+                }
+
+            }
+            return commonDivisor;
         }
+
+        return -1;
     }
 }
