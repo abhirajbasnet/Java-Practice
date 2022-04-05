4ee803196c552af3873c3dca84720a93ffb28d54
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-06 01:17:35 +1000

    completed a project

--- NumberToWords/src/NumberToWords.java
+++ NumberToWords/src/NumberToWords.java
@@ -4,26 +4,36 @@ public class NumberToWords {
 
     public static void main(String[] args) {
 
-        numberToWords(-112);
+        numberToWords(10);
 
         System.out.println("xxxxxxxxxxx");
 
-        System.out.println(reverse(-121));
+        System.out.println(reverse(01));
         System.out.println(reverse(2121));
         System.out.println(reverse(100));
         System.out.println(reverse(4321));
 
         System.out.println("xxxxxxxxxx");
 
+        System.out.println(getDigitCount(123));
+        System.out.println(getDigitCount(0));
+        System.out.println(getDigitCount(-12));
+        System.out.println(getDigitCount(5200));
+
     }
 
     public static void numberToWords(int number) {
 
-        if (number >= 0) {
-            while (number > 0) {
+        int digitCount = getDigitCount(number);
+        number = reverse(number);
+
+        if (number > 0) {
+
+            for (int i = digitCount; i > 0; i--) {
                 int lastDigit = number % 10;
                 number /= 10;
-                //System.out.println(lastDigit);
+
+
                 switch (lastDigit) {
                     case 0:
                         System.out.println("Zero");
@@ -60,7 +70,10 @@ public class NumberToWords {
                         break;
                 }
             }
-        }else
+        }else if(number == 0){
+            System.out.println("Zero");
+        }
+        else
         System.out.println("Invalid Value");
     }
 
@@ -77,6 +90,21 @@ public class NumberToWords {
             //System.out.println(reverseDigit);
         }
         return reverseDigit;
+    }
+
+    public static int getDigitCount(int number){
+
+        int count = 0;
+        if (number > 0){
+            while (number != 0){
+                number /=10;
+                count++;
+            }
+            return count;
+        }else if(number == 0){
+            return 1;
+        }
+        return -1;
     }
 }
 
