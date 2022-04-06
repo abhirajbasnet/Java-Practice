8587d0d4139db17d6ac3e3c238b95a77f038adde
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-06 22:36:40 +1000

    learned a way to change the value of the string to the wrapper class

--- ParsingValueToString/src/javapractice/abhi/Main.java
+++ ParsingValueToString/src/javapractice/abhi/Main.java
@@ -3,6 +3,19 @@ package javapractice.abhi;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String numberAsString = "2018.00";
+        System.out.println("numberAsString= " + numberAsString);
+
+//       int number = Integer.parseInt(numberAsString);
+//        System.out.println("Number = " + number);
+
+        numberAsString += 1;
+//        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        //System.out.println("number = " + number);
+
+        double doubleNumber = Double.parseDouble(numberAsString);
+        System.out.println("DoubleNuber = " + doubleNumber);
     }
 }
