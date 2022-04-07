15cd970c40cae5d05a6bec9052f86f18a34689db
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-07 14:40:26 +1000

    worked on the project

---
+++ whatILearnedSoFar/src/LargestPrime.java
@@ -0,0 +1,27 @@
+public class LargestPrime {
+    public static void main(String[] args) {
+
+        System.out.println(getLargestPrime(20));
+
+    }
+
+    public static int getLargestPrime(int number) {
+
+        if (number <= 0 || number <= 1) {
+            return -1;
+        } else {
+            int largestPrime = 2;
+            while (largestPrime < number) {
+                if (number % largestPrime != 0) {
+                    largestPrime++;
+                    System.out.println(largestPrime);
+                } else {
+                    number = number / largestPrime;
+
+                }
+            }
+            return number;
+        }
+    }
+
+}
