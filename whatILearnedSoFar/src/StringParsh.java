27cdbf5c426a8a000d71f433ca9fb2ac96d7ac58
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-07 13:19:37 +1000

    done about string parsh

---
+++ whatILearnedSoFar/src/StringParsh.java
@@ -0,0 +1,15 @@
+public class StringParsh {
+    public static void main(String[] args) {
+
+        String asString = "1234";
+        System.out.println("asString = " + asString);
+
+        int number = Integer.parseInt(asString);
+        System.out.println("number = "+ number);
+
+
+
+
+    }
+
+}
