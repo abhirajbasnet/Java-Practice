4b0180a72f9ec2afdf3cdcb5de2eb66ec6d86e87
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-07 18:05:44 +1000

    completed a project

---
+++ ReadingUserInputChallenge/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Enumeration;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        int sum = 0;
+
+        while (true) {
+            int order = counter + 1;
+            System.out.println("Enter number #" + order + ":");
+
+            boolean hasInt = scanner.hasNextInt();
+            if (hasInt) {
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+                if (counter == 10) {
+                    break;
+                }
+            } else {
+                System.out.println("Invalid value");
+            }
+            scanner.nextLine(); //handle end of line enter key
+        }
+        System.out.println("Sum = " + sum);
+
+        scanner.close();
+    }
+}
+
