c000f63690c639b8e32a8c1eb6e982e4296bbcba
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-08 12:18:01 +1000

    Worked on oop

---
+++ Class/src/javapractice/abhi/Main.java
@@ -0,0 +1,15 @@
+package javapractice.abhi;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car porshce = new Car();
+        Car holden = new Car();
+        porshce.setModel("911");
+        System.out.println("Model is " + porshce.getModel());
+
+
+    }
+
+}
