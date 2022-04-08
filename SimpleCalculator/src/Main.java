00f339a67073c2b7760bd34a72692e81b624f205
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-08 13:02:26 +1000

    completed a project value

---
+++ SimpleCalculator/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("Add = " + calculator.getAdditionalResult());
+        System.out.println("Subtraction = " + calculator.getSubtractionResult());
+        calculator.setSecondNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("Multiply = " + calculator.getMultiplicationResult());
+        System.out.println("Divide = " + calculator.getDivisionResult());
+
+    }
+}
