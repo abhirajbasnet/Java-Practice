d4227a8c62da7b4f683736493632f3f92100db47
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-09 13:56:39 +1000

    completed a challenge

--- BankAccount/src/Main.java
+++ BankAccount/src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BankAccount bankAccount = new BankAccount();
-        BankAccount abhirajAccount = new BankAccount(123,1000,"Abhiraj","Basnet","myemail@gmail.com","012 3456 789");
+//        BankAccount bankAccount = new BankAccount();
+//        BankAccount abhirajAccount = new BankAccount(123,1000,"Abhiraj","Basnet","myemail@gmail.com","012 3456 789");
 
 //        bankAccount.setFirstName("Abhiraj");
 //        bankAccount.setLastName("Basnet");
@@ -14,8 +14,12 @@ public class Main {
 //        bankAccount.setEmail("myemail@gmail.com");
 //        bankAccount.setPhoneNumber("123456789");
 
-        System.out.println(bankAccount.getName() + "\n" + "Your account balance is " + bankAccount.getBalance() +
-                "\n" + "Your phone number is " + bankAccount.getPhoneNumber() + "\n" + "Your email is " + bankAccount.getEmail() );
+//        System.out.println(bankAccount.getName() + "\n" + "Your account balance is " + bankAccount.getBalance() +
+//                "\n" + "Your phone number is " + bankAccount.getPhoneNumber() + "\n" + "Your email is " + bankAccount.getEmail() );
+
+        System.out.println("xxxxxxxx");
+        VipCustomer ram = new VipCustomer();
+        System.out.println(ram.getName() + " " + ram.getEmailAddress() + " " + ram.getCreditLimit());
 
 
     }
--- BankAccount/src/VipCustomer.java
+++ BankAccount/src/VipCustomer.java
@@ -1,3 +1,34 @@
 public class VipCustomer {
-    
+    private String name;
+    private int creditLimit;
+    private String emailAddress;
+
+    public VipCustomer(){
+        this("default Name", 000, "default email");
+
+    }
+    public VipCustomer(String name,String emailAddress ){
+        this(name, 1000, emailAddress);
+
+    }
+
+    public VipCustomer(String name, int creditLimit, String emailAddress){
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+
 }
