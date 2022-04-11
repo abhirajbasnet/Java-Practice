6b19040a8c042e95c28d7330f87c2533ae4c4a6d
abhirajbasnet <abhirajbasnet11@gmail.com>
2022-04-11 16:41:24 +1000

    created a dog class and added its field

--- Animal/src/Animal.java
+++ Animal/src/Animal.java
@@ -1,2 +1,50 @@
 public class Animal {
+
+    private String name;
+    private int brain;
+    private int body;
+    private int eyes;
+    private int legs;
+    private String animalSound;
+
+    public Animal(String name, int brain, int body, int eyes, int legs, String animalSound){
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.eyes = eyes;
+        this.legs = legs;
+        this.animalSound = animalSound;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getBody(){
+        return body;
+    }
+
+    public int getEyes() {
+        return eyes;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public String getAnimalSound(){
+        return animalSound;
+    }
+
+    public void setAnimalSound(){
+        System.out.println("Animal makes noise");
+    }
+
+    public void move(){
+        System.out.println("Animal.move() called");
+    }
 }
---
+++ Animal/src/Dog.java
@@ -0,0 +1,59 @@
+public class Dog extends Animal {
+
+    private String fur;
+    private int tail;
+    private int ears;
+    private int nose;
+
+    public Dog(String name, int eyes, int legs, String animalSound, String fur, int tail, int ears, int nose) {
+        super(name, 1, 1, eyes, legs, animalSound);
+        this.fur = fur;
+        this.tail = tail;
+        this.ears = ears;
+        this.nose = nose;
+    }
+
+    public String getFur() {
+        return fur;
+    }
+
+    public int getTail() {
+        return tail;
+    }
+
+    public int getEars() {
+        return ears;
+    }
+
+    public int getNose() {
+        return nose;
+    }
+
+    public void move(int speed){
+        move(5);
+        System.out.println("Dog is walking at " + speed);
+    }
+
+    public void run(int speed){
+        move(10);
+        System.out.println("Dog is running at " + speed);
+
+    }
+
+    public void dogSound(String sound){
+
+        dogSound("Bark");
+        System.out.println("Dog sound is called");
+    }
+
+    @Override
+    public void setAnimalSound() {
+        dogSound("Bark");
+        super.setAnimalSound();
+    }
+
+    @Override
+    public void move() {
+        super.move();
+    }
+}
