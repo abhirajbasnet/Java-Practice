b8ad99ffc1eec2b2bb41b55f936d20dbe4294b35
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-15 01:23:54 +1000

    created a class name Cylinder

--- Cylinder/src/Circle.java
+++ Cylinder/src/Circle.java
@@ -1,2 +1,23 @@
 public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        if (radius>=0){
+            this.radius = radius;
+        }else {
+            radius = 0;
+        }
+
+    }
+
+    public double getRadius() {
+        System.out.println("The radius of a cicle is " + radius);
+        return radius;
+    }
+
+    public double getArea(){
+        double area = (radius * radius *Math.PI);
+        System.out.println("The area of the circle is " + area );
+        return area;
+    }
 }
--- Cylinder/src/Main.java
+++ Cylinder/src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(-2.3);
+        circle.getRadius();
+    }
 }
