37f8821f048b9715c8b6f374510e077590edf3a7
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-14 12:18:14 +1000

    created a cat class

---
+++ Animal/src/Cat.java
@@ -0,0 +1,13 @@
+public class Cat extends Animal {
+
+    private String fur;
+    private int teeth;
+    private int tail;
+    private String gender;
+    private int nose;
+
+    public Cat(String name, int brain, int body, int eyes, int legs, String fur,){
+        super(name,brain,body,eyes,legs);
+    }
+
+}
