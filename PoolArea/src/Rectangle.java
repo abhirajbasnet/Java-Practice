796eb81b01dba36c94cefa7a8f6aaa7b2d780886
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-15 01:42:24 +1000

    Added a test code on main java class and created subclass of rectangle

--- PoolArea/src/Rectangle.java
+++ PoolArea/src/Rectangle.java
@@ -1,5 +1,22 @@
 public class Rectangle {
-    public static void main(String[] args) {
-        
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea(){
+
+        return width * length;
     }
 }
