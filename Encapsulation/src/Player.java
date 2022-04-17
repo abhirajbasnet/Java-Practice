44234bf8d3e37c7b2d6181db9f28c0735884b217
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-17 12:52:38 +1000

    encapsulation

--- Encapsulation/src/Player.java
+++ Encapsulation/src/Player.java
@@ -1,2 +1,19 @@
 public class Player {
+    public String fullName;
+    public int health;
+    public String weapon;
+
+
+
+   public void loseHealth(int damage){
+       this.health = this.health -damage;
+       if (this.health <=0){
+           System.out.println("Player knocked out");
+           //Reduce the number of the lies remaining for the player
+       }
+   }
+
+    public int healthRemaining(){
+        return this.health;
+    }
 }
