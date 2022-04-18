c53ca0348ca6a9bbbf2d8756ad0a42e8c66276a2
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-19 00:23:51 +1000

    added method in the bedroom class

--- Practice/src/Bed.java
+++ Practice/src/Bed.java
@@ -1,2 +1,17 @@
 public class Bed {
+    private String name;
+    private int sheet;
+    private int pillow;
+    private int quilt;
+
+    public Bed(String name, int sheet, int pillow, int quilt) {
+        this.name = name;
+        this.sheet = sheet;
+        this.pillow = pillow;
+        this.quilt = quilt;
+    }
+
+    public void makeBed(){
+        System.out.println("Making Bed");
+    }
 }
--- Practice/src/Bedroom.java
+++ Practice/src/Bedroom.java
@@ -3,5 +3,27 @@ public class Bedroom {
     private Wall wall2;
     private Wall wall3;
     private Wall wall4;
+    private Ceiling ceiling;
+    private Bed bed;
+    private Lamp lamp;
 
+
+    public Bedroom(Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp) {
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
+        this.ceiling = ceiling;
+        this.bed = bed;
+        this.lamp = lamp;
+    }
+
+    public int getLamp(){
+        lamp.turnOn();
+    }
+
+    public void makeBed(){
+        System.out.println("Bedroom -> Making Bed | ");
+        bed.makeBed();
+    }
 }
