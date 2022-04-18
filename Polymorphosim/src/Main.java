75fa5ededbcccc7fb1d0412febe34dc86e84e90b
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-18 22:32:56 +1000

    polymorphism

--- Polymorphosim/src/Main.java
+++ Polymorphosim/src/Main.java
@@ -1,2 +1,96 @@
-public class Main {
+class Movie{
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot(){
+        return "No plot here";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Jaws extends Movie{
+    public Jaws(){
+        super("Jaws");
+    }
+
+    public String plot(){
+        return "A shark eats lots of people";
+    }
+}
+
+class Independence extends Movie {
+
+    public Independence() {
+        super("Independence Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over planet earth";
+    }
+}
+class MazeRunner extends Movie{
+    public MazeRunner(){
+        super ("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try and escape a maze.";
+    }
+}
+
+class StarWar extends Movie{
+    public StarWar() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial forces try to take over the universe";
+    }
+}
+
+class Forgetable extends Movie{
+    public Forgetable() {
+        super("Forgetable");
+    }
+    //No plot method
+}
+
+
+
+    public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i< 11; i++){
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i + " : " + movie.getName() + "\n" +
+                    "Plot: " + movie.plot() + "\n");
+        }
+    }
+
+    public static Movie randomMovie(){
+        int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random Number generated was: " + randomNumber);
+        switch (randomNumber){
+            case 1:
+                return new Jaws();
+            case 2:
+                return new Independence();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWar();
+            case 5:
+                return new Forgetable();
+        }
+        return null;
+    }
 }
