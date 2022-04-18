4b9662ec0cd804c9eaa77936651f0a7808b1a929
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-19 00:06:40 +1000

    added a method in the lamp class

--- Practice/src/Lamp.java
+++ Practice/src/Lamp.java
@@ -1,2 +1,16 @@
 public class Lamp {
+
+    private String color;
+
+    public Lamp(String color) {
+        this.color = color;
+    }
+
+    public void turnOn(){
+        System.out.println("Lamp is turning on..");
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
