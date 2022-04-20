3576b41c4ba4f741652a5c0b4ad13c2e4c96d226
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-20 21:39:39 +1000

    added a method

--- Challenge exercise polyMorphism/src/Holden.java
+++ Challenge exercise polyMorphism/src/Holden.java
@@ -1,2 +1,14 @@
-public class Holden {
+public class Holden extends Car {
+    public Holden() {
+        super(6, "Holden");
+    }
+    public String startEngine(){
+        return getClass().getSimpleName() + " -> start Engine()";
+    }
+    public String accelerate(){
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+    public String brake(){
+        return getClass().getSimpleName() + " -> brake()";
+    }
 }
