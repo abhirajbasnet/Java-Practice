f333355b6912ca3e6f9a61dfc4a4170f81e7756d
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-20 21:36:52 +1000

    added a method in the sub class mitsubishi class

--- Challenge exercise polyMorphism/src/Mitsubishi.java
+++ Challenge exercise polyMorphism/src/Mitsubishi.java
@@ -1,2 +1,14 @@
-public class Mitsubishi {
+public class Mitsubishi extends Car {
+    public Mitsubishi() {
+        super(6, "Mitsubishi");
+    }
+    public String startEngine(){
+        return getClass().getSimpleName() + " -> start Engine()";
+    }
+    public String accelerate(){
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+    public String brake(){
+        return getClass().getSimpleName() + " -> brake()";
+    }
 }
