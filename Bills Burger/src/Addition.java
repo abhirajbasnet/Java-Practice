43b3708f7e94d8e5e2eb8429eba010f5e36e69b6
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-21 15:05:19 +1000

    added a method name addition

---
+++ Bills Burger/src/Addition.java
@@ -0,0 +1,16 @@
+public class Addition {
+    private String addition;
+
+    public Addition(String addition) {
+        this.addition = addition;
+    }
+
+    public String getAddition() {
+        return addition;
+    }
+
+    public void setAddition(String addition) {
+        this.addition = addition;
+    }
+
+}
