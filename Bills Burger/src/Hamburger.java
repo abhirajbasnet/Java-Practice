904679404bad8b5f87e42d5fddb176bdb483a7cf
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-21 15:01:20 +1000

    added a methodin the main class

--- Bills Burger/src/Hamburger.java
+++ Bills Burger/src/Hamburger.java
@@ -1,2 +1,27 @@
 public class Hamburger {
+    private String breadRollType;
+    private String meatType;
+//    private String addition1; // lettuce
+//    private String addition2; // tomato
+//    private String addition3; // carrot
+//    private String addition4; // onion
+    private int price;
+
+    public Hamburger(String breadRollType, String meatType, int price) {
+        this.breadRollType = breadRollType;
+        this.meatType = meatType;
+        this.price = price;
+    }
+
+    public String getBreadRollType() {
+        return breadRollType;
+    }
+
+    public String getMeatType() {
+        return meatType;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
