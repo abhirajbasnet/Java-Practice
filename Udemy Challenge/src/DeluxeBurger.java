9476fd7e7cb4bf25e94b54892f8a72b920d1bc63
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-23 23:09:12 +1000

    completetd a project

--- Udemy Challenge/src/DeluxeBurger.java
+++ Udemy Challenge/src/DeluxeBurger.java
@@ -1,2 +1,28 @@
-public class DeluxeBurger {
+public class DeluxeBurger extends Hamburger{
+    public DeluxeBurger() {
+        super("Deluxe Burger", "Sausage & Beacon ", 14.50, "White");
+        super.addHamburgerAddition1("Chips", 2.79);
+        super.addHamburgerAddition2("Drink",1.81);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Can not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+
+        System.out.println("Can not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Can not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Can not add additional items to a deluxe burger");
+    }
 }
