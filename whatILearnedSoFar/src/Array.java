178bb2cf67e3ee14c968ca40fc16bd8d3622c399
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-24 16:02:40 +1000

    created a two field

--- whatILearnedSoFar/src/Array.java
+++ whatILearnedSoFar/src/Array.java
@@ -1,3 +1,21 @@
 public class Array {
-   
+    //Array are used to store the multiples values in the single variable, instead of the declaring separate variables for each variable
+    //To declare an array define the variable type with square brackets
+
+    private String[] studentName = {"Hari Regmi", "Harry Porter", "Christiano Ronaldo", "Lionel Messi"};
+    private int[] studentRollNumber = {1,2,3,4,5,6,7,8;
+
+    public Array(String[] studentName, int[] studentRollNumber) {
+        this.studentName = studentName;
+        this.studentRollNumber = studentRollNumber;
+    }
+
+    public String[] getStudentName() {
+        return studentName;
+    }
+
+    public int[] getStudentRollNumber() {
+        return studentRollNumber;
+    }
+    
 }
