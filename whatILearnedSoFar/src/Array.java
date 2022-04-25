5f7a8e9903ab867d3497801b3c2a1221bd1c8e07
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-25 22:59:35 +1000

    working on the project

--- whatILearnedSoFar/src/Array.java
+++ whatILearnedSoFar/src/Array.java
@@ -2,10 +2,13 @@ public class Array {
     //Array are used to store the multiples values in the single variable, instead of the declaring separate variables for each variable
     //To declare an array define the variable type with square brackets
 
-    private String[] studentName = {"Hari Regmi", "Harry Porter", "Christiano Ronaldo", "Lionel Messi"};
-    private int[] studentRollNumber = {1,2,3,4,5,6,7,8;
+    private String[] studentName;
+    private int[] studentRollNumber;
 
     public Array(String[] studentName, int[] studentRollNumber) {
+        //{"Hari Regmi", "Harry Porter", "Christiano Ronaldo", "Lionel Messi"}
+        //{1,2,3,4,5,6,7,8}
+        
         this.studentName = studentName;
         this.studentRollNumber = studentRollNumber;
     }
@@ -17,5 +20,9 @@ public class Array {
     public int[] getStudentRollNumber() {
         return studentRollNumber;
     }
-    
+
+    public void getStudentNameAndRollNumber(){
+        System.out.println("The Name of the Student is " + this.studentName + " and the Student Roll Number is " + this.studentRollNumber);
+    }
 }
+
