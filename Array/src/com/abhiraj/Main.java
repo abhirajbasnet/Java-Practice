ad93e1a0ebe299648fdcceb476433f4486239a5a
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-27 20:46:13 +1000

    working on an array

--- Array/src/com/abhiraj/Main.java
+++ Array/src/com/abhiraj/Main.java
@@ -1,8 +1,38 @@
 package com.abhiraj;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+
+        int[] myInteger = getInteger(5);
+
+
+//        int[] myIntArray = new int[10]; //{1,2,3,4,4,5,6,7,5,4,2,};// new int [10] means it hold 10 no of integer in the array
+//        //can be also be written as
+//        /*
+//        my Variable = new int [10];
+//         */
+//        myIntArray[5] = 50; // which means the array 5 gets the value 50
+//        double[] myDoubleArray = new double[10];
+//        System.out.println(myIntArray[5]);
+//
+//        for (int i = 0; i < myIntArray.length; i++) {
+//            myIntArray[i] = i * 100;
+//            printArray(myIntArray);
+//        }
+//    }
+//
+//        public static void printArray( int[] array){
+//            for (int i = 0; i < array.length; i ++){
+//                System.out.println("Element " + i + ", value = " + array[i]);
+//            }
+//        }
+
+
     }
+
 }
