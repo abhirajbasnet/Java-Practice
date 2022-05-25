d9dcf890abde372d1844288551b8d974fc6608d1
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-05-25 23:56:51 +1000

    people

--- Array/src/People.java
+++ Array/src/People.java
@@ -1,4 +1,12 @@
 public class People {
     private String Name;
+    //Java array are used to store multiples values in single variables
+    private String[] cars;
 
+    public People(String name) {
+        Name = name;
+        String[] cars={"Volvo","Honda", "BMW" };
+        int[] myNum = {1,2,3,4,4};
+
+    }
 }
